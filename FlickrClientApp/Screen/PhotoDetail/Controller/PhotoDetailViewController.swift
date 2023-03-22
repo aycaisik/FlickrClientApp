02453db08389b3386ea026ec790b7fec4bcd3da4
Ayça Işık <117450558+aycaisik@users.noreply.github.com>
2023-03-23 02:44:25 +0300

    App interface created.

---
+++ FlickrClientApp/Screen/PhotoDetail/Controller/PhotoDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PhotoDetailViewController.swift
+//  FlickrClientApp
+//
+//  Created by Ayça Işık on 23.03.2023.
+//
+
+import UIKit
+
+class PhotoDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title =  "Photo Detail"
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
